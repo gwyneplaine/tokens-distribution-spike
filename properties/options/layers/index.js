b6ef98fd0bb4d372e3b0a6404330e103c3a5ebeb
gwyneplaine <cc.lee@live.com.au>
2019-04-24 14:20:54 +1000

    layer tokens

---
+++ properties/options/layers/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  layer100: { value: 100 },
+  layer300: { value: 300 },
+  layer200: { value: 200 },
+  layer400: { value: 400 },
+  layer500: { value: 500 },
+  layer510: { value: 510 },
+  layer600: { value: 600 },
+  layer700: { value: 700 },
+  layer800: { value: 800 },
+};
