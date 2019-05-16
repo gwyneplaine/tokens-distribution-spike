690d5f32aa6895d2d3eaaf51b22d3ee9b9b8869a
gwyneplaine <cc.lee@live.com.au>
2019-05-16 10:58:20 +1000

    add snapshots for outputs

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,7 +1,41 @@
-var fs = require('fs');
+/// SNAPSHOT TESTS
+const fs = require('fs');
+const util = require('util');
+const globby = require('globby');
+const { execSync } = require('child_process');
+const cases = require('jest-in-case');
+const readFile = util.promisify(fs.readFile);
 
-describe('test test', () => {
-  it('should pass',() => {
-    expect(true);
-  })
+const stripComments = (file) => {
+  return file.replace(/\/\*([^*]|[\r\n]|(\*+([^*\/]|[\r\n])))*\*\/+/gi, '')
+}
+
+describe('on yarn build', () => {
+  beforeAll(() => {
+    execSync('yarn build');
+  });
+  afterAll(() => {
+    console.time('CLEAN');
+    rimraf.sync('packages/*/!(package.json)*/!(package.json)*');
+    console.timeEnd('CLEAN');
+  });
+
+  cases('output files should match snapshots', async opts => {
+    const filePaths = await globby(opts.pattern, { ignore: ['CHANGELOG.md', 'package.json']});
+    await Promise.all(filePaths.map(async filePath => {
+      const file = await readFile(filePath, 'utf-8');
+      return file;
+    })).then(files => {
+      files.forEach(file => {
+        expect(stripComments(file)).toMatchSnapshot();
+      })
+    });
+  }, [
+    { pattern: './packages/design-tokens-css/*.css', name: 'css' },
+    { pattern: './packages/design-tokens-scss/*.scss', name: 'scss' },
+    { pattern: './packages/design-tokens-less/*.less', name: 'less' },
+    { pattern: './packages/design-tokens-js/src/*.js', name: 'js' },
+    { pattern: './packages/design-tokens-json/*/*', name: 'json' },
+  ]);
+
 })
